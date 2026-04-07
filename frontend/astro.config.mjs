2b2c7efcf05d78fba667f968999a03595f749c40
A01745597 <A01745597@itesm.mx>
2026-04-06 21:05:11 -0600

    Frontend/chore/main structure and styles working

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/vite';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwind()],
+  },
+  adapter: node({
+    mode: "standalone"
+  }),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://talenthackathon-economity-backend-production.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  // Opcional: si usas 'hybrid' o 'server' para el admin en el futuro
+  output: 'server', 
 });
