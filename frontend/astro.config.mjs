d97310220c014f4b964b86e8aad5ce3b3e7899ed
A01745597 <A01745597@itesm.mx>
2026-04-07 13:06:03 -0600

    Login testing

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@tailwindcss/vite';
 import node from "@astrojs/node";
+import node from '@astrojs/node'
+import clerk from '@clerk/astro'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +11,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwind()],
   },
-  adapter: node({
-    mode: "standalone"
-  }),
+  integrations: [clerk()],
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
   server: {
     proxy: {
       '/api': {
